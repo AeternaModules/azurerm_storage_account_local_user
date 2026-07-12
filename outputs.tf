543661a85ef4c93dfdeec54aff430d4c47df6eda
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,44 +1,39 @@
-output "storage_account_local_users" {
-  description = "All storage_account_local_user resources"
-  value       = azurerm_storage_account_local_user.storage_account_local_users
-  sensitive   = true
-}
 output "storage_account_local_users_home_directory" {
-  description = "List of home_directory values across all storage_account_local_users"
-  value       = [for k, v in azurerm_storage_account_local_user.storage_account_local_users : v.home_directory]
+  description = "Map of home_directory values across all storage_account_local_users, keyed the same as var.storage_account_local_users"
+  value       = { for k, v in azurerm_storage_account_local_user.storage_account_local_users : k => v.home_directory }
 }
 output "storage_account_local_users_name" {
-  description = "List of name values across all storage_account_local_users"
-  value       = [for k, v in azurerm_storage_account_local_user.storage_account_local_users : v.name]
+  description = "Map of name values across all storage_account_local_users, keyed the same as var.storage_account_local_users"
+  value       = { for k, v in azurerm_storage_account_local_user.storage_account_local_users : k => v.name }
 }
 output "storage_account_local_users_password" {
-  description = "List of password values across all storage_account_local_users"
-  value       = [for k, v in azurerm_storage_account_local_user.storage_account_local_users : v.password]
+  description = "Map of password values across all storage_account_local_users, keyed the same as var.storage_account_local_users"
+  value       = { for k, v in azurerm_storage_account_local_user.storage_account_local_users : k => v.password }
   sensitive   = true
 }
 output "storage_account_local_users_permission_scope" {
-  description = "List of permission_scope values across all storage_account_local_users"
-  value       = [for k, v in azurerm_storage_account_local_user.storage_account_local_users : v.permission_scope]
+  description = "Map of permission_scope values across all storage_account_local_users, keyed the same as var.storage_account_local_users"
+  value       = { for k, v in azurerm_storage_account_local_user.storage_account_local_users : k => v.permission_scope }
 }
 output "storage_account_local_users_sid" {
-  description = "List of sid values across all storage_account_local_users"
-  value       = [for k, v in azurerm_storage_account_local_user.storage_account_local_users : v.sid]
+  description = "Map of sid values across all storage_account_local_users, keyed the same as var.storage_account_local_users"
+  value       = { for k, v in azurerm_storage_account_local_user.storage_account_local_users : k => v.sid }
   sensitive   = true
 }
 output "storage_account_local_users_ssh_authorized_key" {
-  description = "List of ssh_authorized_key values across all storage_account_local_users"
-  value       = [for k, v in azurerm_storage_account_local_user.storage_account_local_users : v.ssh_authorized_key]
+  description = "Map of ssh_authorized_key values across all storage_account_local_users, keyed the same as var.storage_account_local_users"
+  value       = { for k, v in azurerm_storage_account_local_user.storage_account_local_users : k => v.ssh_authorized_key }
 }
 output "storage_account_local_users_ssh_key_enabled" {
-  description = "List of ssh_key_enabled values across all storage_account_local_users"
-  value       = [for k, v in azurerm_storage_account_local_user.storage_account_local_users : v.ssh_key_enabled]
+  description = "Map of ssh_key_enabled values across all storage_account_local_users, keyed the same as var.storage_account_local_users"
+  value       = { for k, v in azurerm_storage_account_local_user.storage_account_local_users : k => v.ssh_key_enabled }
 }
 output "storage_account_local_users_ssh_password_enabled" {
-  description = "List of ssh_password_enabled values across all storage_account_local_users"
-  value       = [for k, v in azurerm_storage_account_local_user.storage_account_local_users : v.ssh_password_enabled]
+  description = "Map of ssh_password_enabled values across all storage_account_local_users, keyed the same as var.storage_account_local_users"
+  value       = { for k, v in azurerm_storage_account_local_user.storage_account_local_users : k => v.ssh_password_enabled }
 }
 output "storage_account_local_users_storage_account_id" {
-  description = "List of storage_account_id values across all storage_account_local_users"
-  value       = [for k, v in azurerm_storage_account_local_user.storage_account_local_users : v.storage_account_id]
+  description = "Map of storage_account_id values across all storage_account_local_users, keyed the same as var.storage_account_local_users"
+  value       = { for k, v in azurerm_storage_account_local_user.storage_account_local_users : k => v.storage_account_id }
 }
 
